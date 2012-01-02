2b2ff0c1e37208af00ef6d706e84982fc5a274e1
Diveader <o40@mail.ru>
2012-01-02 07:26:07 +0200

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Oleg Chorny"
+  user.email                 "o40@mail.ru"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
