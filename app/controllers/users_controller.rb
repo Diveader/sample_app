3f88d78f1a869befe2ff2e5a483dcf15b7b190c9
Diveader <o40@mail.ru>
2012-01-12 17:57:37 +0200

    bugfix with /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
   end
 
   def admin_user
-	  redirect_to(root_path) unless current_user.admin?
+	  redirect_to(root_path) unless current_user && current_user.admin?
   end
 end
